e84b79fa532de534d6ed6a5f03185e4d71b39a98
Alberto Ruiz <aruiz@redhat.com>
2026-04-12 11:06:01 +0100

    initramfs: add kunit tests for unpack_to_rootfs consumed parameter
    Test the new consumed out-parameter added to unpack_to_rootfs() for
    EROFS initrd layer scanning.  Cover all boundary combinations:
    cpio with/without trailer, cpio+junk, cpio+NUL-padding+junk,
    concatenated cpios, cpio+EROFS-boundary NULs, pure junk, all-NULs,
    empty buffer, and exact-fit archives.

--- init/initramfs_internal.h
+++ init/initramfs_internal.h
@@ -5,4 +5,11 @@
 char *unpack_to_rootfs(char *buf, unsigned long len, unsigned long *consumed);
 #define CPIO_HDRLEN 110
 
+#ifdef CONFIG_INITRD_EROFS
+#define EROFS_BLKSZBITS_MIN 9 /* 512 bytes */
+#define EROFS_BLKSZBITS_MAX 30 /* 1 GiB */
+
+unsigned long try_parse_erofs(void *buf, unsigned long off, unsigned long len);
+#endif
+
 #endif
